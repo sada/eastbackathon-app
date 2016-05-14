adad0aa0f9ea3966ecf27da121115f19d4cd25b4
Akihiro Sada <sada@netlab.jp>
2016-05-14 17:02:18 +0900

    横スクロール画面を追加

---
+++ javascripts/main.js
@@ -0,0 +1,141 @@
+enchant();
+
+window.onload = function() {
+  var game_ = new Game(320, 320);
+  game_.fps = 24;
+  game_.preload('./javascripts/enchant_js-0.8.3/images/start.png', './javascripts/enchant_js-0.8.3/images/gameover.png', './javascripts/enchant_js-0.8.3/images/chara1.png', './images/bg1.png', './images/bg2.png', './javascripts/enchant_js-0.8.3/images/chara6.png');
+
+  game_.onload = function() {
+    var createStartScene = function() {
+      var scene = new Scene();
+      scene.backgroundColor = '#fcc800';
+
+      var startImage = new Sprite(236, 48);
+      startImage.image = game_.assets['./javascripts/enchant_js-0.8.3/images/start.png'];
+      startImage.x = 42;
+      startImage.y = 157;
+      scene.addChild(startImage);
+
+      startImage.addEventListener(Event.TOUCH_START, function(e) {
+          game_.replaceScene(createGameScene());
+      });
+
+      return scene;
+    };
+
+    var createGameScene = function() {
+        var scroll = 0;
+
+        var GROUND_LINE = 250;
+        var SCROLL_SPEED = 10;
+
+        var scene = new Scene();
+        scene.backgroundColor = '#8cc820';
+
+        var bg1 = new Sprite(320, 320);
+        bg1.image = game_.assets['./images/bg1.png'];
+        bg1.x = 0;
+        bg1.y = 0;
+        scene.addChild(bg1);
+
+        var bg2 = new Sprite(320, 320);
+        bg2.image = game_.assets['./images/bg2.png'];
+        bg2.x = 320;
+        bg2.y = 0;
+        scene.addChild(bg2);
+
+        var charactor = new Sprite(32, 32);
+        charactor.image = game_.assets['./javascripts/enchant_js-0.8.3/images/chara1.png'];
+        charactor.x = 80;
+        charactor.y = GROUND_LINE - charactor.height;
+        scene.addChild(charactor);
+
+        var charactorHit = new Sprite(1, 1);
+        charactorHit.x = charactor.x + charactor.width / 2;
+        charactorHit.y = charactor.y + charactor.height / 2;
+        scene.addChild(charactorHit);
+
+        var enemy = new Sprite(32, 32);
+        enemy.image = game_.assets['./javascripts/enchant_js-0.8.3/images/chara6.png'];
+        enemy.x = -enemy.width;
+        enemy.y = GROUND_LINE - enemy.height;
+        scene.addChild(enemy);
+
+        var charactorDead = function() {
+          charactor.frame = 3;
+          game_.pushScene(createGameoverScene(scroll));
+        }
+
+        scene.addEventListener(Event.ENTER_FRAME, function(){
+            scroll += SCROLL_SPEED;
+
+            if (scroll % 640 === 0) {
+              enemy.x = 320;
+            }
+
+            if (enemy.x > -enemy.width) {
+              enemy.x -= SCROLL_SPEED;
+              if (enemy.intersect(charactorHit)) {
+                charactorDead();
+              }
+            }
+
+            charactor.frame ++;
+            if (charactor.frame > 2) {
+              charactor.frame = 0;
+            }
+
+            charactorHit.x = charactor.x + charactor.width/2;
+            charactorHit.y = charactor.y + charactor.height/2;
+
+            bg1.x -= SCROLL_SPEED;
+            bg2.x -= SCROLL_SPEED;
+
+            if (bg1.x <= -320) {
+                bg1.x = 320;
+            }
+            if (bg2.x <= -320) {
+                bg2.x = 320;
+            }
+        });
+
+        return scene;
+    }
+
+    var createGameoverScene = function(scroll) {
+      var scene = new Scene();
+      scene.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+
+      var gameoverImage = new Sprite(189, 97);
+      gameoverImage.image = game_.assets['./javascripts/enchant_js-0.8.3/images/gameover.png'];
+      gameoverImage.x = 66;
+      gameoverImage.y = 170;
+      scene.addChild(gameoverImage);
+
+      var retryLabel = new Label('リトライ');
+      retryLabel.width = 320;
+      retryLabel.textAlign = 'center';
+      retryLabel.color = '#ffffff';
+      retryLabel.x = 0;
+      retryLabel.y = 284;
+      retryLabel.font = '24px sans-serif';
+      scene.addChild(retryLabel);
+
+      var buttonRetry = new Sprite(320, 32);
+      buttonRetry.x = 0;
+      buttonRetry.y = 284;
+      scene.addChild(buttonRetry);
+
+      buttonRetry.addEventListener(Event.TOUCH_END, function(){
+        game_.popScene();
+        game_.replaceScene(createStartScene());
+      });
+
+      return scene;
+    };
+
+    game_.replaceScene(createStartScene());
+  }
+
+  game_.start();
+}
